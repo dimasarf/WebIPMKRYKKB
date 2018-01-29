9cb8071ee6f0bc6fbcb896d5fbb48337f0aaeabf
dimasarf <dimas.arifin97@yahoo.com>
2018-01-29 07:43:48 +0700

    Tampilan Registrasi

---
+++ resources/views/HalamanAwal.blade.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>IPMKRY-KKB</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS Hari2 CSS -->
+    <base href="{{URL::asset('/')}}" target="_blank">
+
+      <link rel="stylesheet" href="/css/bootstrap.min.css">
+      <link rel="stylesheet" href="/css/IPMKRY-KKB2.css">
+
+
+  </head>
+  <body>
+    <!-- CSS END -->
+  <div class="IPMKRY-KKB-nav">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark IPMKRY-KKB-blue">
+      <nav class="navbar navbar-dark IPMKRY-KKB-blue">
+        <a class="navbar-brand" href="#">
+          <img src="/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">IPMKRY-KKB</a>
+    </nav>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+              <a class="nav-link" href="#" onclick="document.getElementById('contact-us').style.display='block'">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup>Alumni</a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">2014</a>
+                <a class="dropdown-item" href="#">2013</a>
+                <a class="dropdown-item" href="#">2012</a>
+                <a class="dropdown-item" href="#">2011</a>
+                <a class="dropdown-item" href="#">2010</a>
+                <a class="dropdown-item" href="#">2009</a>
+                <a class="dropdown-item" href="#">2008</a>
+                <a class="dropdown-item" href="#">2007</a>
+                <a class="dropdown-item" href="#">2006</a>
+                <a class="dropdown-item" href="#">2005</a>
+                <a class="dropdown-item" href="#">2004</a>
+              </div>
+            </li>
+          </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn my-2 my-sm-0 fa fa-search" type="submit"></button>
+        </form>
+      </div>
+    </nav>
+  </div>
+
+<!-- Content -->
+<div class="IPMKRY-KKB-content">
+  <div class="IPMKRY-KKB-slides">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="https://2.bp.blogspot.com/-ssDyOIQwe8Q/WjqI5idT_cI/AAAAAAAAAHk/mKAua1DQh3Eve6oSpF29fD1uCJ3zFFbvACLcBGAs/s1600/hhhghghghghkkkkkkkkkkkkkkkkkkkkkkkzaqwe%2B1.jpg" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="https://2.bp.blogspot.com/-ssDyOIQwe8Q/WjqI5idT_cI/AAAAAAAAAHk/mKAua1DQh3Eve6oSpF29fD1uCJ3zFFbvACLcBGAs/s1600/hhhghghghghkkkkkkkkkkkkkkkkkkkkkkkzaqwe%2B1.jpg" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="https://2.bp.blogspot.com/-ssDyOIQwe8Q/WjqI5idT_cI/AAAAAAAAAHk/mKAua1DQh3Eve6oSpF29fD1uCJ3zFFbvACLcBGAs/s1600/hhhghghghghkkkkkkkkkkkkkkkkkkkkkkkzaqwe%2B1.jpg" alt="Third slide">
+          </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+  </div>
+  <div class="IPMKRY-KKB-cards">
+    <div class="card text-black bg-info mb-3" style="max-width: 18rem;">
+      <div class="card-header">Dapatkan informasi terbaru!</div>
+        <div class="card-body">
+          <h5 class="card-title">LOGIN</h5>
+          <p class="card-text">Login untuk mendapatkan informasi seputar event IPMKRY-KKB di-Yogyakarta</p>
+          <a href="#" class="btn btn-primary fa fa-lock"> Login</a>
+        </div>
+      </div>
+      <div class="card text-black bg-info mb-3" style="max-width: 18rem;">
+      <div class="card-header">Bergabung sekarang!</div>
+        <div class="card-body">
+          <h5 class="card-title">SIGN-UP</h5>
+          <p class="card-text">Sign Up untuk jadi anggota dan dapatkan kartu anggota IPMKRY-KKB</p>
+          <a href="#" class="btn btn-primary fa fa-users"> Sign Up</a>
+        </div>
+      </div>
+  </div>
+  <div class="IPMKRY-KKB-definisi">
+    <div class="judul">
+      <h1>IPMKRY-KKB</h1>
+      <h5><i>"Ikatan Pelajar Mahasiswa Kepulauan Riau Yogyakarta - Komisariat Batam"</i></h5>
+    </div>
+      <div class="isi">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+  </div>
+  <div class="gallery-h1">
+    <h1>Gallery</h1>
+    <h5><i>"Album kegiatan IPMKRY-KKB"</i></h5>
+  </div>
+  <div class="IPMKRY-KKB-gallery">
+    <div id="galleryIPMKRY-KKB" class="next">
+        <div class="gambar1">
+          <a href="#">
+            <h1>Logo IPMKRY-KKB</h1>
+          </a>
+        </div>
+        <div class="gambar1">
+          <a href="#">
+            <h1>Logo IPMKRY-KKB</h1>
+          </a>
+        </div>
+        <div class="gambar1">
+          <a href="#">
+            <h1>Logo IPMKRY-KKB</h1>
+          </a>
+        </div>
+        <div class="gambar1">
+          <a href="#">
+            <h1>Logo IPMKRY-KKB</h1>
+          </a>
+        </div>
+        <div class="gambar1">
+          <a href="#">
+            <h1>Logo IPMKRY-KKB</h1>
+          </a>
+        </div>
+        <div class="gambar1">
+          <a href="#">
+            <h1>Logo IPMKRY-KKB</h1>
+          </a>
+        </div>
+        <div class="gambar1">
+          <a href="#">
+            <h1>Logo IPMKRY-KKB</h1>
+          </a>
+        </div>
+        <div class="gambar1">
+          <a href="#">
+            <h1>Logo IPMKRY-KKB</h1>
+          </a>
+        </div>
+        <div class="gambar1">
+          <a href="#">
+            <h1>Logo IPMKRY-KKB</h1>
+          </a>
+        </div>
+        <div class="gambar1">
+          <a href="#">
+            <h1>Logo IPMKRY-KKB</h1>
+          </a>
+        </div>
+        <div class="gambar1">
+          <a href="#">
+            <h1>Logo IPMKRY-KKB</h1>
+          </a>
+        </div>
+        <div class="gambar1">
+          <a href="#">
+            <h1>Logo IPMKRY-KKB</h1>
+          </a>
+        </div>
+        <div class="gambar1">
+          <a href="#">
+            <h1>Logo IPMKRY-KKB</h1>
+          </a>
+        </div>
+        <div class="gambar1">
+          <a href="#">
+            <h1>Logo IPMKRY-KKB</h1>
+          </a>
+        </div>
+        <div class="gambar1">
+          <a href="#">
+            <h1>Logo IPMKRY-KKB</h1>
+          </a>
+        </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal -->
+
+<div id="contact-us" class="IPMKRY-KKB-modal">
+    <div class="IPMKRY-KKB-modal-content IPMKRY-KKB-animate-top IPMKRY-KKB-card-4 IPMKRY-KKB-teal">
+      <header class="IPMKRY-KKB-container IPMKRY-KKB-teal IPMKRY-KKB-center2 IPMKRY-KKB-padding-32">
+        <span onclick="document.getElementById('contact-us').style.display='none'"
+       class="IPMKRY-KKB-button IPMKRY-KKB-teal IPMKRY-KKB-xlarge IPMKRY-KKB-display-topright">X</span>
+        <h2 class="IPMKRY-KKB-wide IPMKRY-KKB-center">CONTACT</h2>
+        <p class="IPMKRY-KKB-opacity IPMKRY-KKB-center"><i>"Feel free to contact"</i></p>
+    <div class="IPMKRY-KKB-row IPMKRY-KKB-padding-32">
+      <div class="IPMKRY-KKB-col m6 IPMKRY-KKB-large IPMKRY-KKB-margin-bottom IPMKRY-KKB-center">
+        <i class="fa fa-map-marker" style="width:30px"></i> Yogyakarta, Indonesia<br>
+        <i class="fa fa-phone" style="width:30px"></i> Phone: +6281276060044<br>
+        <i class="fa fa-envelope" style="width:30px"> </i> Email: throwbackltd@gmail.com<br>
+      </div>
+      <div class="IPMKRY-KKB-col m6">
+        <form action="/action_page.php" target="_blank">
+          <div class="IPMKRY-KKB-row-padding" style="margin:0 -16px 8px -16px">
+            <div class="IPMKRY-KKB-half">
+              <input class="IPMKRY-KKB-input IPMKRY-KKB-border" type="text" placeholder="Name" required name="Name">
+            </div>
+            <div class="IPMKRY-KKB-half">
+              <input class="IPMKRY-KKB-input IPMKRY-KKB-border" type="text" placeholder="Email" required name="Email">
+            </div>
+          </div>
+          <input class="IPMKRY-KKB-input IPMKRY-KKB-border" type="text" placeholder="Message" required name="Message">
+           <button class="IPMKRY-KKB-button IPMKRY-KKB-black IPMKRY-KKB-section IPMKRY-KKB-right" type="submit">SEND</button>
+        </form>
+      </div>
+    </div>
+    </div>
+  </div>
+
+
+<!-- Modal End -->
+
+<!-- Footer  -->
+<footer class="IPMKRY-KKB-foooter">
+  <div class="kotak">
+    <div class="IPMKRY-KKB-social-media">
+      <div class="content-social-media">
+        <div class="h1-block">
+          <h1>About</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+      <div class="content-social-media1">
+        <div class="h1-block">
+          <h1>Department</h1>
+          <li><a href="#">PSDM</a></li>
+          <li><a href="#">Humas</a></li>
+          <li><a href="#">Keagamaan</a></li>
+          <li><a href="#">Kewirausahaan</a></li>
+          <li><a href="#">Keolahragaan</a></li>
+          <li><a href="#">Seni & Budaya</a></li>
+        </div>
+      </div>
+      <div class="content-social-media2">
+        <div class="h1-block">
+          <h1>Pengurus Inti</h1>
+          <li><a href="#">Ketua:</a></li>
+          <li><a href="#">Wakil:</a></li>
+          <li><a href="#">Sekertaris:</a></li>
+          <li><a href="#">Bendahara:</a></li>
+        </div>
+      </div>
+    </div>
+    <div class="IPMKRY-KKB-help">
+        <h1>Fell Free to Contact Us</h1>
+        <li><i class="fa fa-envelope"> Email: ipmkry-kkb@gmail.com</i></li>
+        <li><i class="fa fa-map-marker"> Address: 14 Seturan Baru Street, Yogyakarta, Indonesia</i></li>
+        <li><i class="fa fa-phone"> Phone: 021-000</i></li>
+        <div class="content-help">
+          <div class="ch">
+            <li><a class="fa fa-facebook" href="#"></a></li>
+          </div>
+          <div class="ch">
+            <li><a class="fa fa-instagram" href="#"></a></li>
+          </div>
+          <div class="ch">
+            <li><a class="fa fa-twitter" href="#"></a></li>
+          </div>
+        </div>
+    </div>
+    <div class="IPMKRY-KKB-copyright"><a href="#"></a>
+      <h3>Â©2018 IKATAN PELAJAR MAHASISWA KEPULAUAN RIAU YOGYAKARTA - KOMISARIAT KOTA BATAM</h3>
+    </div>
+  </div>
+</footer>
+<!-- Footer End -->
+
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+    <script>
+// When the user clicks anywhere outside of the modal, close it
+    var modal = document.getElementById('ticketModal');
+    window.onclick = function(event) {
+    if (event.target == modal) {
+    modal.style.display = "none";
+      }
+    }
+  </script>
+  </body>
+</html>
---
+++ storage/framework/views/93f73701b2500b35f1cd98c2bc7dca9fdb79726d.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Registrasi</title>
+    <base href="<?php echo e(URL::asset('/')); ?>" target="_blank">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+  </head>
+  <body style="background-color:#2d3436" style="width:400px;">
+    <div class="container" >
+      <div class="row  mt-4 mr-4 ml-4 mb-4">
+        <div class="col-xs-12 col-md-12 col-lg-12">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+
+              <div class="modal-header">
+                  <img src="Logo.jpg" alt="" class="mx-auto d-block" width="150px;">
+
+                <br>
+
+              </div>
+
+              <div class="modal-body">
+                <h4 class="text-center">Registrasi Anggota IPMKRY-KKB</h4>
+                <br>
+                <form>
+                  <div class="form-group">
+                    <label for="NamaLengkap" class="control-label">Nama Lengkap</label>
+                    <input type="text" class="form-control" id="NamaLengkap">
+                  </div>
+
+                  <div class="form-group">
+                    <label for="TTL" class="control-label">Tanggal Lahir</label>
+                    <input type="text" class="form-control" id="TTL" placeholder="dd/mm/yyyy">
+                  </div>
+
+                  <div class="form-group">
+                    <label for="Universitas" class="control-label">Universitas</label>
+                    <input type="text" class="form-control" id="Universitas">
+                  </div>
+
+                  <label for="message-text" class="control-label">Jenis Kelamin</label>
+
+                  <div class="form-group mr-1">
+                      <label class="radio-inline">
+                        <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> Laki-Laki
+                      </label>
+                      <label class="radio-inline offset-1">
+                        <input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> Perempuan
+                      </label>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="AlamatJogja" class="control-label">Alamat di Yogyakarta</label>
+                    <textarea class="form-control" id="AlamatJogja"></textarea>
+                  </div>
+
+
+
+                </form>
+              </div>
+
+            <div class="modal-footer">
+              <button type="button" class="btn btn-primary btn-block">Submit</button>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+      </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="<?php echo e(url('js/bootstrap.min.js')); ?>"></script>
+  </body>
+</html>
